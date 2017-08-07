1da11725555c5786e9d7ddcdd6ef05a983149880
DiyaVasu <30623691+DiyaVasu@users.noreply.github.com>
2017-08-07 20:02:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,3 +21,19 @@ if(request.status === 200){
  request.open('GET','http://diyavasu08.imad.hasura-app.io/counter',true);
  request.send(null);
 };
+//submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+
+// make a request to the server and send the name
+
+// capture a list of names and render itas a list
+var names = ['name1','name2','name3','name4'];
+var list = '';
+for (var i=0; i<names.length; i++){
+list += '<li>' +names[i] +'</li>';
+}
+var ul=document.getElementById('namelist');
+ul.innerHTML =list;
+};
